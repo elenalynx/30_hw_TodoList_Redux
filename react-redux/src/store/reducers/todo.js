8aed78ac409d070fcec4aba4c609c04f6d1cf512
elenalynx <elnlynx@gmail.com>
2023-02-16 20:24:35 +0200

    add chek

--- react-redux/src/store/reducers/todo.js
+++ react-redux/src/store/reducers/todo.js
@@ -12,19 +12,19 @@ const DEFAULT_TODO = {
 }
 const initialState = {
     list: [
-        { 'title': 'ipsam XXX', 'status': true, 'done': false, 'id': '1' },
-        { 'title': 'ratione nam alias', 'status': true, 'done': false, 'id': '2' },
-        { 'title': 'atque non dolore', 'status': true, 'done': true, 'id': '3' },
-        { 'title': 'in quos exercitationem', 'status': true, 'done': false, 'id': '4' },
-        { 'title': 'quia perferendis ipsam', 'status': true, 'done': false, 'id': '5' },
-        { 'title': 'commodi harum commodi', 'status': false, 'done': false, 'id': '6' },
-        { 'title': 'ipsam occaecati nulla', 'status': false, 'done': false, 'id': '7' },
-        { 'title': 'nemo soluta tempora', 'status': false, 'done': false, 'id': '8' },
+        {'title': 'ipsam XXX', 'status': true, 'done': false, 'id': '1'},
+        {'title': 'ratione nam alias', 'status': true, 'done': false, 'id': '2'},
+        {'title': 'atque non dolore', 'status': true, 'done': true, 'id': '3'},
+        {'title': 'in quos exercitationem', 'status': true, 'done': false, 'id': '4'},
+        {'title': 'quia perferendis ipsam', 'status': true, 'done': false, 'id': '5'},
+        {'title': 'commodi harum commodi', 'status': false, 'done': false, 'id': '6'},
+        {'title': 'ipsam occaecati nulla', 'status': false, 'done': false, 'id': '7'},
+        {'title': 'nemo soluta tempora', 'status': false, 'done': false, 'id': '8'},
     ],
     editTodo: DEFAULT_TODO,
 }
 
-export default function todoReducer (state = initialState, { type, payload }) {
+export default function todoReducer(state = initialState, {type, payload}) {
     switch (type) {
         case TODO_CREATE_ACTION:
             if (state.editTodo.id) { // edit
@@ -35,18 +35,24 @@ export default function todoReducer (state = initialState, { type, payload }) {
                     editTodo: DEFAULT_TODO,
                 }
             } else { // create
-                return { ...state, list: [ ...state.list, payload ] }
+                return {...state, list: [...state.list, payload]}
             }
-        case TODO_REMOVE_ACTION: return { ...state, list: state.list.filter(todo => todo.id !== payload) }
-        case TODO_EDIT_ACTION: return { ...state, editTodo: payload }
+        case TODO_REMOVE_ACTION:
+            return {...state, list: state.list.filter(todo => todo.id !== payload)}
+        case TODO_EDIT_ACTION:
+            return {...state, editTodo: payload}
         case TODO_CHECK_ACTION:
-            const editTodo = state.editTodo.done === false ? {...state.editTodo, done: true} : {...state.editTodo, done: false};
-            return { ...state, list: state.list.map(todoItem => todoItem.id === editTodo.id ? editTodo : todoItem)} ;
+            const newTodo = {
+                ...payload,
+                done: payload.done === false,
+            };
 
-            // if (state.editTodo.done === true) return { ...state, list: [...state.list, payload] }
-            // else return { ...state, list: [...state.list, payload] }
+            // console.log(payload)
+            // console.log(newTodo)
 
-            // return { ...state, editTodo: payload }
-        default: return state;
+            return {...state, list: state.list.map(todoItem => todoItem.id === newTodo.id ? newTodo : todoItem)};
+
+        default:
+            return state;
     }
 }
